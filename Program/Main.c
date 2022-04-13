4691f94fef6f1f3fe3fc358e50cd5073e3e6b3bb
Dodjebullet <101244413+Dodjebullet@users.noreply.github.com>
2022-04-13 14:54:40 +0300

    Add files via upload

---
+++ Program/Main.c
@@ -0,0 +1,44 @@
+
+#include <bur/plctypes.h>
+
+#ifdef _DEFAULT_INCLUDES
+	#include <AsDefault.h>
+#endif
+
+void _INIT ProgramInit(void)
+{
+
+}
+
+void _CYCLIC ProgramCyclic(void)
+{
+//	led1=1;
+//	led2=1;
+//	led3=1;
+//	led4=1;
+//	for(t=0;t<=6;t=t+1){
+//		if(t==1){
+//			led1=1;
+//		}
+//		if(t==2){
+//			led2=1;
+//		}
+//		if(t==3){
+//			led3=1;
+//		}
+//		if(t==4){
+//			led4=1;
+//		}
+//		if(t==5){
+//			t=0;
+//		}
+//		
+//	}
+
+}
+
+void _EXIT ProgramExit(void)
+{
+
+}
+
